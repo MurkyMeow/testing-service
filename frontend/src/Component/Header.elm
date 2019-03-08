ab8d74ce1c96080df40840f1d91e6b6e1f4bc6cd
MurkyMeow <dinosowermurky@gmail.com>
2019-03-08 23:01:43 +0900

    frontend: +Header component

---
+++ frontend/src/Component/Header.elm
@@ -0,0 +1,14 @@
+module Component.Header exposing (view)
+
+import Component.Button as Button
+import Html exposing (div, a, text)
+import Html.Attributes exposing (class, href)
+
+view =
+  div [ class "Header" ]
+    [ div [ class "logo" ] [ text "Hello world" ]
+    , div [ class "nav" ]
+        [ Button.regular "Вход"
+        , Button.link "/signin" "Регистрация"
+        ]
+    ]
--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -2,6 +2,7 @@ import Browser exposing (sandbox)
 import Html exposing (div, text)
 
 import Page.Index as Page
+import Component.Header as Header
 
 main =
   sandbox { init = 0, update = update, view = view }
@@ -11,5 +12,6 @@ update msg model =
 
 view model =
   div []
-  [ Page.view
+  [ Header.view
+  , Page.view
   ]
