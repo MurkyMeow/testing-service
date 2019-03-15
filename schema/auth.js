dbab528b28c7a75f79942ff4148b6165b1416c71
MurkyMeow <dinosowermurky@gmail.com>
2019-03-15 22:20:05 +0900

    backend/auth: use shorthand for function declarations

--- schema/auth.js
+++ schema/auth.js
@@ -5,14 +5,14 @@ const env = require('../env');
 const { APIError, assert } = require('../util/error');
 
 module.exports = {
-  signin: async ({ email, password }) => {
+  async signin({ email, password }) {
     const [user] = await db('Accounts').where({ email });
     assert(user, new APIError(400, 'Invalid login'));
     assert(bcrypt.compareSync(password, user.password), new APIError(400, 'Invalid password'));
     const token = jwt.sign({ id: user.id }, env.secret, { expiresIn: '24h' });
     return token;
   },
-  signup: async ({ email, password }) => {
+  async signup({ email, password }) {
     const [user] = await db('Accounts').where({ email });
     assert(!user, new APIError(400, 'That email is busy'));
     const hash = bcrypt.hashSync(password, 8);
