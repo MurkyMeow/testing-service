5cf80eab5e9e11b2e60a74f7832fbf2096cf02ec
MurkyMeow <dinosowermurky@gmail.com>
2019-03-15 15:24:36 +0900

    frontend/model: show condition

--- frontend/src/Component/Modal.elm
+++ frontend/src/Component/Modal.elm
@@ -1,7 +1,9 @@
 module Component.Modal exposing(view)
 
-import Html exposing (div)
+import Html exposing (div, text)
 import Html.Attributes exposing (class)
 
-view modal =
-  div [ class "_modal" ] [ modal ]
+view open modal =
+  if open then
+    div [ class "_modal" ] [ modal ]
+  else text ""
