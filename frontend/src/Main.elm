56c1e3962d29d72efda57557c5e200190cb24696
MurkyMeow <dinosowermurky@gmail.com>
2019-03-15 16:22:44 +0900

    frontend: moved the form and the header into Main (trying to follow the rule of not creating extra modules and not die at the same time)

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,40 +1,111 @@
 import Browser exposing (element)
-import Html exposing (Html, div, text)
-import Html.Attributes exposing (rel, href)
+import Html exposing (Html, div, text, form, input)
+import Html.Attributes exposing (rel, href, class, placeholder, required, type_, value)
+import Html.Events exposing (onClick, onSubmit, onInput)
 import Platform.Cmd as Cmd
-import Tuple
+import Http
+import Json.Encode as Encode
+import Json.Decode as Decode
 
 import Page.Index as Page
-import Component.Header as Header
-import Form.Auth as AuthForm
+import Component.Button as Button
+import Component.Modal as Modal
+
+-- TODO: decompose Msg Model and update into small pieces?
 
 type alias Model =
-  { loginForm : AuthForm.Model
+  { signupOpen : Bool
+  , signinOpen : Bool
+  , email : String
+  , password : String
+  , message : String
   }
 
+type Modal
+  = Signin
+  | Signup
+
 type Msg
-  = LoginForm AuthForm.Msg
+  = SetOpenState Modal Bool
+  | SetEmail String
+  | SetPassword String
+  | Submit
+  | Response (Result Http.Error String)
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ({ loginForm = AuthForm.Model "" "" "" }, Cmd.none)
+  ({ email = ""
+   , password = ""
+   , signupOpen = False
+   , signinOpen = False
+   , message = ""
+   }
+  , Cmd.none
+  )
 
 main =
   element { init = init, update = update, view = view, subscriptions = \_ -> Sub.none }
 
+submit =
+  let
+    body = Encode.object
+      [ ("query", Encode.string "{ hello }")
+      ]
+      |> Http.jsonBody
+  in
+    Http.post
+      { url = "http://localhost:3000/api"
+      , body = body
+      , expect = Http.expectJson Response (Decode.field "data" (Decode.field "hello" Decode.string))
+      }
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    LoginForm formMsg ->
-      let
-        upd = AuthForm.update formMsg model.loginForm
-      in
-        (Model <| Tuple.first upd, Cmd.map LoginForm <| Tuple.second upd)
+    SetOpenState modal state ->
+      case modal of
+        Signin ->
+          ({ model | signupOpen = state }, Cmd.none)
+        Signup ->
+          ({ model | signinOpen = state }, Cmd.none)
+    SetEmail email ->
+      ({ model | email = email }, Cmd.none)
+    SetPassword password ->
+      ({ model | password = password }, Cmd.none)
+    Response result ->
+      case result of
+        Ok message ->
+          ({ model | message = message }, Cmd.none)
+        _ ->
+          ({ model | message = "Ошибка" }, Cmd.none)
+    Submit ->
+      (model, submit)
 
 view : Model -> Html Msg
 view model =
   div []
-  [ Html.map LoginForm (Header.view model.loginForm)
-  , Page.view
-  , Html.node "link" [ rel "stylesheet", href "/main.css" ] []
-  ]
+    [ viewHeader model.signinOpen model.signupOpen
+    , Page.view
+    , text model.message
+    , Html.node "link" [ rel "stylesheet", href "/main.css" ] []
+    ]
+
+viewHeader signinOpen signupOpen =
+  div [ class "_header" ]
+    [ div [ class "logo" ] [ text "Hello world" ]
+    , div [ class "nav" ]
+        [ Button.view [ onClick (SetOpenState Signup True) ] "Создать аккаунт"
+        , Button.view [ onClick (SetOpenState Signin True) ] "Войти"
+        ]
+    , Modal.view signinOpen viewSignupForm
+    ]
+
+viewSignupForm =
+  div [ class "_auth" ]
+    [ div [ class "header" ] [ text "Заполните поля" ]
+    , form [ onSubmit Submit ]
+        [ input [ placeholder "Email", onInput SetEmail, required True ] []
+        , input [ placeholder "Пароль", onInput SetPassword, required True ] []
+        , input [ class "submit", type_ "submit", value "Войти" ] []
+        ]
+    ]
