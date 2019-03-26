0768e62eb82108a43ff0f24ee46bc13085759a96
MurkyMeow <42230707+MurkyMeow@users.noreply.github.com>
2019-03-26 18:48:09 +1000

    backend/util/auth: fix typo
    Co-Authored-By: ckieverentineas <36624728+ckieverentineas@users.noreply.github.com>

--- util/auth.js
+++ util/auth.js
@@ -3,7 +3,7 @@ const env = require('../env');
 
 const authorizedOnly = resolver => (root, args, context) => {
   const { token } = context.headers;
-  if (jwt.verufy(token, env.secret)) {
+  if (jwt.verify(token, env.secret)) {
     return resolver(root, args, context);
   } throw new Error('Frobidden');
 };
