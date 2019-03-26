1e753e242b48872e22b070b19e1e097d7f9a4818
ckieverentineas <ckieverentineas@yandex.ru>
2019-03-26 19:52:10 +1000

    backend: corrected the syntax

--- util/auth.js
+++ util/auth.js
@@ -5,7 +5,8 @@ const authorizedOnly = resolver => (root, args, context) => {
   const { token } = context.headers;
   if (jwt.verify(token, env.secret)) {
     return resolver(root, args, context);
-  } throw new Error('Frobidden');
+  }
+  throw new Error('Access denied');
 };
 
 module.exports = authorizedOnly;
