790b015ceafc8454eb86a5f00dcf895e42bf0e79
ckieverentineas <ckieverentineas@yandex.ru>
2019-03-27 23:30:04 +1000

    backend/testing: added routes for tests and categories

--- schema/testing.js
+++ schema/testing.js
@@ -23,38 +23,24 @@ module.exports = {
       text: String
     }
 
-    type Answer {
-      id: Int
-      question_id: Int
-      text: String
-      flag: Boolean
-    }
-
     extend type Query {
-      category: [Category]
+      categories: [Category]
       tests: [Test]
       questions(test_id: Int): [Question]
-      answers(question_id: Int): [Answer]
       test(id: Int): Test
     }
     `,
   resolvers: {
     Query: {
-      category: authorizedOnly(async () => {
-        const category = await db('Category');
-        assert(category.length, new APIError(400, 'Category not found'));
-        return category;
+      categories: authorizedOnly(async () => {
+        const categories = await db('Category');
+        assert(categories.length, new APIError(400, 'Category not found'));
+        return categories;
       }),
       tests: authorizedOnly(async () => {
         const tests = await db('Tests');
         assert(tests.length, new APIError(400, 'Tests not found'));
         return tests;
-      }),
-      answers: authorizedOnly(async (_, { question_id }) => {
-        const answers = await db('Answers').where({ question_id });
-        assert(answers.length, new APIError(400, 'Answers not found'));
-        console.log(answers);
-        return answers;
       }),
       test: authorizedOnly(async (_, { id }) => {
         const [test] = await db('Tests').where({ id });
