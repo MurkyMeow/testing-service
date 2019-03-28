a20ae096a8cb91525c2955b6f8ebee1b6a393559
MurkyMeow <dinosowermurky@gmail.com>
2019-03-28 16:19:03 +0900

    frontend/util/auth: add userid to the context

--- util/auth.js
+++ util/auth.js
@@ -5,8 +5,8 @@ const { APIError } = require('./error');
 const authorizedOnly = resolver => (root, args, context) => {
   const { token } = context.headers;
   try {
-    jwt.verify(token, env.secret);
-    return resolver(root, args, context);
+    const { id } = jwt.verify(token, env.secret);
+    return resolver(root, args, { ...context, userid: id });
   } catch (err) {
     throw new APIError(403, 'Access denied');
   }
