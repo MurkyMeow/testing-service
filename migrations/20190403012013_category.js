11d4ed7455871e93b09eae68817fa50ebd43c6e1
MurkyMeow <dinosowermurky@gmail.com>
2019-04-03 01:22:22 +0900

    migrations: +category table

---
+++ migrations/20190403012013_category.js
@@ -0,0 +1,8 @@
+exports.up = knex =>
+  knex.schema.createTable('category', table => {
+    table.increments('id').primary();
+    table.string('name');
+  });
+
+exports.down = knex =>
+  knex.schema.dropTable('category');
