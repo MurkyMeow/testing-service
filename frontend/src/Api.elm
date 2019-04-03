434d72fd4f3ce1c51a9aaad9239827d356e3f267
MurkyMeow <dinosowermurky@gmail.com>
2019-04-03 21:12:18 +0900

    frontend: add Api module

---
+++ frontend/src/Api.elm
@@ -0,0 +1,41 @@
+module Api exposing (Category, getCategories)
+
+import Http
+import Json.Encode as Encode
+import Json.Decode as Decode
+
+apiEndpoint =
+  "http://localhost:4000"
+
+query msg decoder body =
+  let
+    encodedBody = Encode.object
+      [ ("query", Encode.string body)
+      ]
+      |> Http.jsonBody
+  in
+    Http.post
+      { url = apiEndpoint
+      , body = encodedBody
+      , expect = Http.expectJson msg (Decode.field "data" decoder)
+      }
+
+type alias Category =
+  { id : Int
+  , name : String
+  }
+
+categoriesDecoder =
+  Decode.field "categories" (Decode.list
+    (Decode.map2 Category
+      (Decode.field "id" Decode.int)
+      (Decode.field "name" Decode.string)
+    )
+  )
+
+getCategories msg =
+  query msg categoriesDecoder """
+    categories {
+      name
+    }
+  """
