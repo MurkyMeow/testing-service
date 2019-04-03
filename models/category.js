4652b57324387762984da35c29c4466f0b84e500
MurkyMeow <dinosowermurky@gmail.com>
2019-04-03 17:19:47 +0900

    backend/model/category: add relation mappings

--- models/category.js
+++ models/category.js
@@ -1,7 +1,21 @@
 const { Model } = require('objection');
+const Test = require('./test');
 
 module.exports = class extends Model {
   static get tableName() {
     return 'category';
+  }
+
+  static get relationMappings() {
+    return {
+      tests: {
+        relation: Model.HasManyRelation,
+        modelClass: Test,
+        join: {
+          from: 'category.id',
+          to: 'test.category_id'
+        }
+      }
+    };
   }
 };
