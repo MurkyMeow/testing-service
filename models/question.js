66a5d1df169160a922099f9b6224b7fc0515ba8a
MurkyMeow <dinosowermurky@gmail.com>
2019-04-03 17:23:52 +0900

    backend/model/question: add relation mappings

--- models/question.js
+++ models/question.js
@@ -1,7 +1,21 @@
 const { Model } = require('objection');
+const Answer = require('./answer');
 
 module.exports = class extends Model {
   static get tableName() {
     return 'question';
+  }
+
+  static get relationMappings() {
+    return {
+      answers: {
+        relation: Model.HasManyRelation,
+        modelClass: Answer,
+        join: {
+          from: 'question.id',
+          to: 'answer.question_id'
+        }
+      }
+    };
   }
 };
