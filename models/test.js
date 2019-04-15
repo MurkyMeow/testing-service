e83633bd25fd8ac9f57746ef91ec0384d51f034f
MurkyMeow <dinosowermurky@gmail.com>
2019-04-15 17:33:28 +0900

    backend/model/test: add `getUserResult` function

--- models/test.js
+++ models/test.js
@@ -1,8 +1,20 @@
 const { Model } = require('objection');
+const Progress = require('./progress');
 
 module.exports = class extends Model {
   static get tableName() {
     return 'test';
+  }
+
+  async getUserResult(user_id) {
+    const questions = await this.$relatedQuery('questions')
+      .eager('answers')
+      .modifyEager('answers', builder => builder.where({ correct: 1 }));
+    const rightAnswers = questions.reduce((acc, el) => [...acc, ...el.answers], []);
+    const userAnswers = await Progress.query().where({ user_id });
+    const correctAmount = rightAnswers
+      .reduce((acc, el) => (userAnswers.find(x => x.id === el.id) ? acc + 1 : acc), 0);
+    return correctAmount / rightAnswers.length;
   }
 
   static get relationMappings() {
