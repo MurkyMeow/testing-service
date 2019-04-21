48ee88800546c5458417bc72192d192966d4ee9f
MurkyMeow <dinosowermurky@gmail.com>
2019-04-21 16:42:47 +0900

    frontend/api: remake everything according to the new REST APi

--- frontend/src/Api.elm
+++ frontend/src/Api.elm
@@ -16,20 +16,26 @@ import Json.Encode as Encode
 import Json.Decode as Decode
 
 apiEndpoint =
-  "http://localhost:4000/graphql"
+  "http://localhost:4000"
 
-query body vars decoder msg =
+get endpoint params decoder msg =
   let
-    encodedBody = Encode.object
-      [ ("query", Encode.string body)
-      , ("variables", Encode.object vars)
-      ]
-      |> Http.jsonBody
+    foo =
+      List.foldr (\(key, value) acc -> acc ++ key ++ "=" ++ value ++ "&") "" params
+  in
+    Http.get
+      { url = apiEndpoint ++ endpoint ++ "?" ++ foo
+      , expect = Http.expectJson msg decoder
+      }
+
+post endpoint body decoder msg =
+  let
+    encodedBody = Http.jsonBody (Encode.object body)
   in
     Http.post
-      { url = apiEndpoint
+      { url = apiEndpoint ++ endpoint
       , body = encodedBody
-      , expect = Http.expectJson msg (Decode.field "data" decoder)
+      , expect = Http.expectJson msg decoder
       }
 
 type alias Category =
@@ -38,14 +44,7 @@ type alias Category =
   }
 
 getCategories =
-  query
-  """{
-    categories {
-      id
-      name
-    }
-  }"""
-  []
+  get "/test/categories" []
   (Decode.field "categories" (Decode.list
     (Decode.map2 Category
       (Decode.field "id" Decode.int)
@@ -59,22 +58,14 @@ type alias Test =
   }
 
 getTests categoryId =
-  query
-  """
-  query ($category_id: Int!) {
-    tests(category_id: $category_id) {
-      id
-      name
-    }
-  }
-  """
-  [ ("category_id", Encode.int categoryId) ]
-  (Decode.field "tests" (Decode.list
-    (Decode.map2 Test
-      (Decode.field "id" Decode.int)
-      (Decode.field "name" Decode.string)
-    )
-  ))
+  get "/test/tests"
+    [("category_id", String.fromInt categoryId)]
+    (Decode.field "tests" (Decode.list
+      (Decode.map2 Test
+        (Decode.field "id" Decode.int)
+        (Decode.field "name" Decode.string)
+      )
+    ))
 
 type alias Answer =
   { id : Int
@@ -88,62 +79,38 @@ type alias Question =
   }
 
 getQuestions testId =
-  query
-  """
-  query ($test_id: Int!) {
-    questions(test_id: $test_id) {
-      id
-      text
-      answers {
-        id
-        text
-      }
-    }
-  }
-  """
-  [ ("test_id", Encode.int testId) ]
-  (Decode.field "questions" (Decode.list
-    (Decode.map3 Question
-      (Decode.field "id" Decode.int)
-      (Decode.field "text" Decode.string)
-      (Decode.field "answers" (Decode.list
-        (Decode.map2 Answer
-          (Decode.field "id" Decode.int)
-          (Decode.field "text" Decode.string)
-        )
-      ))
-    )
-  ))
+  get "/test/questions"
+    [ ("test_id", String.fromInt testId) ]
+    (Decode.field "questions" (Decode.list
+      (Decode.map3 Question
+        (Decode.field "id" Decode.int)
+        (Decode.field "text" Decode.string)
+        (Decode.field "answers" (Decode.list
+          (Decode.map2 Answer
+            (Decode.field "id" Decode.int)
+            (Decode.field "text" Decode.string)
+          )
+        ))
+      )
+    ))
 
 type alias User =
   { name : String
   }
 
 signin email password =
-  query
-  """
-  query ($email: String!, $password: String!) {
-    signin(email: $email, password: $password) {
-      name
-    }
-  }
-  """
-  [ ("email", Encode.string email)
-  , ("password", Encode.string password)
-  ]
-  (Decode.field "signin" (Decode.map User
-    (Decode.field "name" Decode.string)
-  ))
+  post "/auth/signin"
+    [ ("email", Encode.string email)
+    , ("password", Encode.string password)
+    ]
+    (Decode.map User
+      (Decode.field "name" Decode.string)
+    )
 
 signup name email password =
-  query
-  """
-  query ($name: String!, $email: String!, $password: String!) {
-    signup(name: $name, email: $email, password: $password)
-  }
-  """
-  [ ("name", Encode.string name)
-  , ("email", Encode.string email)
-  , ("password", Encode.string password)
-  ]
-  (Decode.field "signup" Decode.string)
+  post "/auth/signup"
+    [ ("name", Encode.string name)
+    , ("email", Encode.string email)
+    , ("password", Encode.string password)
+    ]
+    (Decode.field "signup" Decode.string)
