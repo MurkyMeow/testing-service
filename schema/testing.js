7b7695be08b73d99d6ea3ac60d4322c79939f402
MurkyMeow <dinosowermurky@gmail.com>
2019-04-30 16:10:44 +0900

    backend/testing: retrun arrays directly

--- schema/testing.js
+++ schema/testing.js
@@ -8,15 +8,15 @@ const Progress = require('../models/progress');
 module.exports = (fastify, opts, next) => {
   fastify.get('/categories', async () => {
     const categories = await Category.query();
-    return { categories };
+    return categories;
   });
   fastify.get('/questions', async ({ query }) => {
     const questions = await Question.query().where({ test_id: query.test_id }).eager('answers');
-    return { questions };
+    return questions;
   });
   fastify.get('/tests', async ({ query }) => {
     const tests = await Test.query().where({ category_id: query.category_id }).eager('questions');
-    return { tests };
+    return tests;
   });
   fastify.get('/result', async ({ query, session }) => {
     const [test] = await Test.query().where({ id: query.test_id });
