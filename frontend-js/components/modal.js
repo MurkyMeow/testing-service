dc8c1deb041087c964bc98a6a81a103f01980431
MurkyMeow <dinosowermurky@gmail.com>
2019-05-01 14:46:36 +0900

    frontend-js/modal: return array instead of object

--- frontend-js/components/modal.js
+++ frontend-js/components/modal.js
@@ -4,17 +4,16 @@ const useModal = initial => {
   const [modalOpen, setModalOpen] = useState(initial);
   const showModal = () => setModalOpen(true);
   const hideModal = () => setModalOpen(false);
-  return {
-    showModal,
-    hideModal,
-    modal: el(content => html`
+  return [
+    el(content => html`
     <div class=${`modal ${modalOpen ? '' : 'modal-hidden'}`} onclick=${hideModal}>
       <div class="modal-content" onclick=${e => e.stopPropagation()}>
         ${content}
       </div>
     </div>
-    `)
-  };
+    `),
+    showModal
+  ];
 };
 
 export default useModal;
