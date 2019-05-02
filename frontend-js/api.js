c3fef1e83a32aff87a74820fb8d09d09faf1472a
MurkyMeow <dinosowermurky@gmail.com>
2019-05-02 13:56:26 +0900

    frontend-js: include credentials

--- frontend-js/api.js
+++ frontend-js/api.js
@@ -1,5 +1,8 @@
 export const get = (url, options) =>
-  fetch(`http://localhost:4000${url}`, options)
+  fetch(`http://localhost:4000${url}`, {
+    ...options,
+    credentials: 'include',
+  })
     .then(res => res.json());
 
 export const post = (url, data) =>
