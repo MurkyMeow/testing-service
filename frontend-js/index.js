864e757cd0724ec0cf7fab3f9e5024856e053157
MurkyMeow <dinosowermurky@gmail.com>
2019-05-02 13:57:12 +0900

    frontend: add global state

--- frontend-js/index.js
+++ frontend-js/index.js
@@ -1,3 +1,33 @@
 const { default: el, html, useState, useEffect } = window.neverland;
 
-export { el, html, useState, useEffect };
+const state = {
+  user: null
+};
+
+const listeners = Object
+  .keys(state)
+  .reduce((acc, key) => ({ ...acc, [key]: [] }), {});
+
+const useGlobalState = key => {
+  if (!(key in state)) throw Error(`State does not contain this key: ${key}`);
+  const listener = listeners[key];
+  const [value, setValue] = useState(state[key]);
+  useEffect(() => {
+    listener.push(setValue);
+    return () => listener.splice(listener.indexOf(setValue), 1);
+  });
+  const update = newValue => {
+    state[key] = newValue;
+    listener.forEach(handler => handler(newValue));
+  };
+  return [value, update];
+};
+
+export {
+  el,
+  html,
+  useState,
+  useEffect,
+  state,
+  useGlobalState,
+};
