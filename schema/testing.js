5aeb92fd4781aa467fb1a23d71e515ce3dd122d3
MurkyMeow <dinosowermurky@gmail.com>
2019-05-03 13:19:35 +0900

    backend: add Rest function to automatically create boilerplate routes

--- schema/testing.js
+++ schema/testing.js
@@ -1,37 +1,38 @@
-// const authorizedOnly = require('../util/auth');
-const router = require('koa-joi-router');
 const Category = require('../models/category');
 const Test = require('../models/test');
 const Question = require('../models/question');
 const Progress = require('../models/progress');
+const { Rest } = require('../util/server');
 
-const testing = router();
-
-testing.get('/categories', async ctx => {
-  const categories = await Category.query();
-  ctx.body = categories;
+const rest = Rest('/test');
+rest.register('/categories', Category);
+rest.register('/questions', Question, {
+  get: {
+    where: query => ({ test_id: query.test_id }),
+  },
+  put: {
+    verify: async body => {
+      const items = await Test.query().where({ id: body.test_id });
+      return items.length > 0;
+    },
+  },
 });
-testing.get('/questions', async ctx => {
-  const questions = await Question.query().where({ test_id: ctx.query.test_id }).eager('answers');
-  ctx.body = questions;
+rest.register('/tests', Test, {
+  get: {
+    where: query => ({ category_id: query.category_id }),
+  },
 });
-testing.get('/tests', async ctx => {
-  const tests = await Test.query().where({ category_id: ctx.query.category_id }).eager('questions');
-  ctx.body = tests;
-});
-testing.get('/result', async ctx => {
+rest.router.get('/result', async ctx => {
   const [test] = await Test.query().where({ id: ctx.query.test_id });
-  ctx.body = { result: test.getUserResult(ctx.session.userid) };
+  ctx.body = { result: test.getUserResult(ctx.session.user.id) };
 });
-testing.post('/answer', async ctx => {
+rest.router.post('/answer', async ctx => {
   const { answer_ids } = ctx.query;
   const [question] = await Question.query().where({ id: ctx.query.question_id });
   ctx.assert(await question.validateAnswers(answer_ids), 400, 'Some of the answers are invalid');
-  const rows = answer_ids.map(id => ({ user_id: ctx.session.userid, answer_id: id }));
+  const rows = answer_ids.map(id => ({ user_id: ctx.session.user.id, answer_id: id }));
   await Progress.query().insertGraph(rows);
   ctx.body = { answer: 'Ok' };
 });
 
-testing.prefix('/test');
-
-module.exports = testing;
+module.exports = rest.router;
---
+++ util/server.js
@@ -0,0 +1,42 @@
+const Router = require('koa-joi-router');
+
+const standard = {
+  get: {
+    where: query => ({ id: query.id })
+  },
+  put: {
+    verify: () => true,
+  }
+};
+
+module.exports.Rest = prefix => {
+  const router = Router();
+  router.prefix(prefix);
+  return {
+    router,
+    register: (name, model, options = {}) => {
+      router.get(name, async ctx => {
+        const { where } = options.get || standard.get;
+        const { id, eager } = ctx.request.query;
+        if (id) {
+          ctx.body = await model.query().where(where(ctx.request.query)).eager(eager);
+        } else {
+          ctx.body = await model.query().eager(eager);
+        }
+      });
+      router.put(name, async ctx => {
+        const { verify } = options.put || standard.put;
+        ctx.assert(await verify(ctx.request.body), 409, 'This item already exists');
+        ctx.body = await model.query().insert(ctx.request.body);
+      });
+      router.post(name, async ctx => {
+        const { id, ...patch } = ctx.request.body;
+        ctx.body = await model.query().findById(id).patch(patch);
+      });
+      router.delete(name, async ctx => {
+        const { id } = ctx.request.query;
+        ctx.body = await model.query().deleteById(id);
+      });
+    }
+  };
+};
