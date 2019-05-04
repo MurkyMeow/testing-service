3074e6c20167a5c80da8a81b79237fc05484bcde
MurkyMeow <dinosowermurky@gmail.com>
2019-05-04 13:47:30 +0900

    frontend-js: add an abstraction for list pages

--- frontend-js/pages/categories.js
+++ frontend-js/pages/categories.js
@@ -1,29 +1,16 @@
-import { el, html, useState, useEffect } from '../index.js';
-import { get } from '../api.js';
+import { el, html } from '../index.js';
 import button from '../components/button.js';
+import listPage from '../abstract/list-page.js';
 
-const category = el(({ name, id }) => html`
-  <div class="category">
-    <div class="category-name">${name}</h2>
-    <p>Описание</p>
-    ${button({ link: `#/category/${id}` })('Перейти')}
-  </div>
-`);
-
-const categories = el(() => {
-  const [items, setItems] = useState([]);
-
-  useEffect(async () => {
-    const response = await get('/test/categories');
-    setItems(response);
-  }, []);
-
-  return html`
-  <div class="categories-page">
-    <h1 class="categories-title">Categories page</h1>
-    <div class="categories-list">${items.map(item => category(item))}</div>
-  </div>
-  `;
+export default listPage({
+  title: 'Категории',
+  endpoint: '/test/categories',
+  colWidth: 300,
+  template: el(({ name, id }) => html`
+    <div class="category">
+      <div class="category-name">${name}</h2>
+      <p>Описание</p>
+      ${button({ link: `#/category/${id}` })('Перейти')}
+    </div>
+  `),
 });
-
-export default categories;
