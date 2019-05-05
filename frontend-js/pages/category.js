a361c1d8677b94ff836e8661bc52beac4879dd27
ckieverentineas <ckieverentineas@yandex.ru>
2019-05-05 22:24:11 +1000

    frontend/category: cleanup

--- frontend-js/pages/category.js
+++ frontend-js/pages/category.js
@@ -5,13 +5,11 @@ import listPage from '../abstract/list-page.js';
 export default ({ query }) => listPage({
   title: 'Выберете тест:',
   endpoint: `/test/tests/?${query.id}`,
-  test: console.log(query.id),
   colWidth: 300,
   template: el(({ name, id }) => html`
     <div class="test">
       <div class="test-name">${name}</div>
       <p>Описание</p>
-      <p>${query.id}</p>
       ${button({ link: `#/questions/${id}/` })('Пройти тест')}
     </div>
   `),
