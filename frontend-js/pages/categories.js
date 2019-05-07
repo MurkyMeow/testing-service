fcfa84eb0fb9ff4d387d086d9844b4c333f252d0
MurkyMeow <dinosowermurky@gmail.com>
2019-05-07 14:56:20 +0900

    frontend/categories: improve category look

--- frontend-js/pages/categories.js
+++ frontend-js/pages/categories.js
@@ -5,12 +5,18 @@ import listPage from '../abstract/list-page.js';
 export default listPage({
   title: 'Категории',
   endpoint: '/test/categories',
-  colWidth: 300,
+  colWidth: 500,
   template: el(({ name, id }) => html`
     <div class="category">
-      <div class="category-name">${name}</h2>
-      <p>Описание</p>
-      ${button({ link: `#/category/${id}` })('Перейти')}
+      <div class="category-name">${name}</div>
+      <div class="category-description">Описание</div>
+      <div class="category-summary">
+        <div><i>info</i>6</div>
+        <div><i>query_builder</i>60</div>
+      </div>
+      <div class="category-actions">
+        ${button({ link: `#/category/${id}` })('Перейти')}
+      </div>
     </div>
   `),
 });
