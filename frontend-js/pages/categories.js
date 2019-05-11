d235798db2b1600a9f450532097b22ec8d5d6271
MurkyMeow <dinosowermurky@gmail.com>
2019-05-11 20:29:19 +0900

    frontend-js/categories: make categories display their tests + mockup a button to add a test

--- frontend-js/pages/categories.js
+++ frontend-js/pages/categories.js
@@ -1,4 +1,5 @@
-import { el, html } from '../index.js';
+import { el, html, useEffect, useState } from '../index.js';
+import { get } from '../api.js';
 import button from '../components/button.js';
 import listPage from '../abstract/list-page.js';
 
@@ -6,10 +7,24 @@ export default listPage({
   title: 'Категории',
   endpoint: '/test/categories?samples=10',
   colWidth: 500,
-  template: el(({ name, id }) => html`
+  template: el(({ name, id }) => {
+    const [tests, setTests] = useState([]);
+    useEffect(async () => {
+      try {
+        setTests(await get(`/test/tests?category_id=${id}`));
+      } catch (err) {
+        console.error(err);
+      }
+    }, []);
+    return html`
     <div class="category">
       <div class="category-name">${name}</div>
-      <div class="category-description">Описание</div>
+      <div class="category-test-list">
+        ${el(() => tests.map(test => html`
+          <div class="category-test-list-item">${test.name}</div>
+        `))}
+        <div class="category-test-list-add-btn">Добавить тест</div>
+      </div>
       <div class="category-summary">
         <div><i>info</i>6</div>
         <div><i>query_builder</i>60</div>
@@ -18,5 +33,6 @@ export default listPage({
         ${button({ link: `#/tests/${id}/` })('Перейти')}
       </div>
     </div>
-  `),
+    `;
+  }),
 });
