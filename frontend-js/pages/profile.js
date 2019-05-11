a24838247726324857ea029a864dc062ecedbb54
MurkyMeow <dinosowermurky@gmail.com>
2019-05-11 20:10:43 +0900

    frontend-js/profile: mockup user tests

--- frontend-js/pages/profile.js
+++ frontend-js/pages/profile.js
@@ -1,9 +1,34 @@
-import { el, html } from '../index.js';
+import { el, html, useGlobalState, useEffect, useState } from '../index.js';
+import { get } from '../api.js';
+import button from '../components/button.js';
 
-const profile = el(() => html`
-  <div>
-    <h1>Profile page</h1>
-  </div>
-`);
+const profile = el(() => {
+  const [user] = useGlobalState('user');
+  const [tests, setTests] = useState([]);
+
+  useEffect(async () => {
+    setTests(await get('/test/tests?samples=5'));
+  }, []);
+
+  return html`
+    <div class="profile">
+      <h1>${user ? user.name : ''}</h1>
+      <h2>Тесты:</h2>
+      <div class="profile-tests">
+        ${el(() => tests.map(test => html`
+          <div class="profile-test">
+            <div class="profile-test-name">${test.name}</div>
+            <div class="profile-test-stats">
+              <i>remove_red_eye</i>20
+            </div>
+            <div class="profile-test-actions">
+              ${button({ classname: 'profile-test-btn' })('Пройти')}
+            </div>
+          </div>
+        `))}
+      </div>
+    </div>
+  `;
+});
 
 export default profile;
