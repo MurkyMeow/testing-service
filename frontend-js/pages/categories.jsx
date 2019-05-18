9010bd385eac91bc865661a1106456540618d565
MurkyMeow <dinosowermurky@gmail.com>
2019-05-18 17:07:32 +0900

    frontend-js/categories: redesign and refactor

--- frontend-js/pages/categories.jsx
+++ frontend-js/pages/categories.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import { put } from '../api';
+import { put, get, useRequest } from '../api';
 import Button from '../components/button';
-import ListPage from '../abstract/list-page';
 
 const Categories = () => {
   const [name, setName] = useState('');
+  const [, items = []] = useRequest(() => get('/test/categories?samples=30&eager=tests'));
   const submit = async () => {
     try {
       await put('/test/categories', { name });
@@ -13,33 +13,31 @@ const Categories = () => {
     }
   };
   return (
-    <div>
-      <ListPage title="Категории" colWidth={500} endpoint="/test/categories?samples=30&eager=tests">
-        {category => (
+    <div className="page-categories">
+      <div className="page-title">Категории</div>
+      <div className="page-categories__add">
+        <input className="page-categories__add__input" placeholder="Название категории" onChange={e => setName(e.target.value)}/>
+        <Button className="page-categories__add__btn" onClick={submit}>+</Button>
+      </div>
+      <div className="page-categories__category-list">
+        {items.map(category => (
           <div className="category" key={category.id}>
-            <div className="category-name">{category.name}</div>
-            <div className="category-test-list">
+            <div className="category__name">{category.name}</div>
+            <div className="category__test-list">
               {category.tests.map(test => (
-                <div className="category-test-list-item" key={test.id}>
+                <div className="category__test-list__test" key={test.id}>
                   {test.name}
                 </div>
               ))}
-              <div className="category-test-list-add-btn">Добавить тест</div>
+              <div className="category__test-list__add-btn">Добавить тест</div>
             </div>
-            <div className="category-summary">
+            <div className="category__summary">
               <div><i>info</i>6</div>
               <div><i>query_builder</i>60</div>
-            </div>
-            <div className="category-actions">
               <Button link={`#/tests/${category.id}`}>Перейти</Button>
             </div>
           </div>
-        )}
-      </ListPage>
-      <div className="categories-add">
-        <input className="categories-add-input" placeholder="Название категории"
-          onChange={e => setName(e.target.value)}/>
-        <Button className="categories-add-btn" onClick={submit}>+</Button>
+        ))}
       </div>
     </div>
   );
