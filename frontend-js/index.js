9185f4df197dabf49f16be36e8c56ab4b9ffd0c9
MurkyMeow <dinosowermurky@gmail.com>
2019-05-18 18:27:43 +0900

    frontend-js: add `useDocument` hook

--- frontend-js/index.js
+++ frontend-js/index.js
@@ -1,15 +1,14 @@
 import { useState, useEffect } from 'react';
+import { remove, put, get } from './api';
 
-const state = {
-  user: null
-};
+const state = {};
+const listeners = {};
 
-const listeners = Object
-  .keys(state)
-  .reduce((acc, key) => ({ ...acc, [key]: [] }), {});
-
-const useGlobalState = key => {
-  if (!(key in state)) throw Error(`State does not contain this key: ${key}`);
+const useGlobalState = (key, initial) => {
+  if (!(key in state)) {
+    state[key] = initial;
+    listeners[key] = [];
+  }
   const listener = listeners[key];
   const [value, setValue] = useState(state[key]);
   useEffect(() => {
@@ -23,7 +22,39 @@ const useGlobalState = key => {
   return [value, update];
 };
 
+const useDocument = (endpoint, relation) => {
+  const [items, setItems] = useGlobalState(endpoint, []);
+  const ep = () => {
+    if (endpoint.includes('?')) {
+      return endpoint.endsWith('?')
+        ? endpoint : `${endpoint}&`;
+    }
+    return `${endpoint}?`;
+  };
+  const refresh = () => {
+    const url = `${ep()}samples=30`;
+    get(relation ? `${url}&eager=${relation}` : url)
+      .then(res => setItems(res))
+      .catch(console.error);
+  };
+  const addItem = params => {
+    put(ep(), relation ? { ...params, eager: relation } : params)
+      .then(item => setItems([...items, item]))
+      .catch(console.error);
+  };
+  const removeItem = id => {
+    remove(`${ep()}id=${id}`)
+      .then(() => setItems(items.filter(x => x.id !== id)))
+      .catch(console.error);
+  };
+  useEffect(() => {
+    if (!items.length) refresh();
+  }, []);
+  return { items, addItem, removeItem, refresh };
+};
+
 export {
   state,
   useGlobalState,
+  useDocument,
 };
