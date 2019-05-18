841445850a7c759af7aa742b185f8e8cbed2fc1b
MurkyMeow <dinosowermurky@gmail.com>
2019-05-19 02:22:32 +0900

    backend: add defaults to timestamps in the result table

--- migrations/20190519010520_result.js
+++ migrations/20190519010520_result.js
@@ -4,7 +4,7 @@ exports.up = knex =>
     table.integer('user_id').notNullable().references('id').inTable('user');
     table.integer('test_id').notNullable().references('id').inTable('test');
     table.float('score').notNullable();
-    table.timestamps();
+    table.timestamps(true, true);
   });
 
 exports.down = knex =>
