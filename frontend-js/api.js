74048ab1157fb003f87d998e89c65653c134ff25
MurkyMeow <dinosowermurky@gmail.com>
2019-05-19 20:36:05 +0900

    frontend-js/api: cancel request after unmount

--- frontend-js/api.js
+++ frontend-js/api.js
@@ -25,11 +25,14 @@ export const put = withBody('PUT');
 export const useRequest = (request, params = {}) => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
+  let cancelled = false;
   useEffect(() => {
     request().then(res => {
+      if (cancelled) return;
       setData(params.only && Array.isArray(res) ? res[0] : res);
       setLoading(false);
     }).catch(console.error);
+    return () => cancelled = true;
   }, []);
   return [loading, data];
 };
