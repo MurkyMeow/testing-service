830de94666d87c9576b977ad38d24fe2b62e6d3e
MurkyMeow <dinosowermurky@gmail.com>
2019-05-19 20:47:24 +0900

    frontend-js/category: reposition add-btn and show a message when empty

--- frontend-js/pages/category.jsx
+++ frontend-js/pages/category.jsx
@@ -3,22 +3,25 @@ import { useDocument } from '../index';
 import Button from '../components/button';
 
 const Category = ({ router }) => {
-  const { items, removeItem } = useDocument(`/test/tests?category_id=${router.query.id}`, {
-    samples: '30',
-  });
+  const { items, removeItem } = useDocument(`/test/tests?category_id=${router.query.id}`);
   const addTest = () => router.push(`/test_edit?category_id=${router.query.id}`);
   return (
     <div className="category-page">
-      {items.map(test => (
-        <div className="category-page__test" key={test.id}>
-          <i className="category-page__test__delete-btn" onClick={() => removeItem(test.id)}>close</i>
-          <div className="category-page__test__name">{test.name}</div>
-          <div className="category-page__test__description">Описание</div>
-          <Button className="category-page__link" link={`#/questions/${test.id}/`}>
-            Пройти тест
-          </Button>
-        </div>
-      ))}
+      {!items.length && <>
+        <div className="page-title">Добавить первый тест в этой категории..?</div>
+      </>}
+      <div className="category-page__test-list">
+        {items.map(test => (
+          <div className="category-page__test" key={test.id}>
+            <i className="category-page__test__delete-btn" onClick={() => removeItem(test.id)}>close</i>
+            <div className="category-page__test__name">{test.name}</div>
+            <div className="category-page__test__description">Описание</div>
+            <Button className="category-page__link" link={`#/questions/${test.id}/`}>
+              Пройти тест
+            </Button>
+          </div>
+        ))}
+      </div>
       <Button className="category-page__add-btn" onClick={addTest}>+</Button>
     </div>
   );
