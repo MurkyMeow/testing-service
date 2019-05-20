fb22c48cf8ecc7c17b2f850556ae5c36d88bfcb8
MurkyMeow <dinosowermurky@gmail.com>
2019-05-20 19:35:56 +0900

    frontend-js/test-card: move the link to the stats

--- frontend-js/components/test-card.jsx
+++ frontend-js/components/test-card.jsx
@@ -16,15 +16,15 @@ export const TestCard = ({ className, test, onDelete }) => {
         <i className="test-card__delete-btn" onClick={removeItem}>close</i>
         <div className="test-card__name">{test.name}</div>
         <div className="test-card__description">Описание</div>
-        <Button className="test-card__link" link={`/test?id=${test.id}`}>
-          Пройти тест
-        </Button>
       </div>
       <div className="test-card__stats">
         {test.questions && (
           <div><i>help_outline</i>{test.questions.length}</div>
         )}
         <div><i>how_to_reg</i>76%</div>
+        <Button className="test-card__link" link={`/test?id=${test.id}`}>
+          Пройти тест
+        </Button>
       </div>
     </div>
   );
