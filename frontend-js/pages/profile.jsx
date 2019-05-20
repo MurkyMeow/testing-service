a542fecefca8c850a3dfb97c83618f7d6099d17e
MurkyMeow <dinosowermurky@gmail.com>
2019-05-20 16:43:17 +0900

    frontend-js/profile: add field to alter user name

--- frontend-js/pages/profile.jsx
+++ frontend-js/pages/profile.jsx
@@ -1,15 +1,25 @@
 import { useGlobalState } from '../index';
-import { get, useRequest } from '../api';
+import { get, useRequest, post } from '../api';
 import Button from '../components/button';
 
 const Profile = () => {
-  const [user] = useGlobalState('user');
+  const [user, setUser] = useGlobalState('user');
   const [, tests = []] = useRequest(() => get('/test/tests?samples=5'));
   const [, finished = []] = useRequest(() => get('/stats/tests'));
 
+  const changeName = async e => {
+    const { value } = e.target;
+    if (user.name !== value) {
+      await post('/stats/name/', { name: value });
+      setUser({ ...user, name: value });
+    }
+  };
   return (
     <div className="profile">
-      <h1>{user ? user.name : ''}</h1>
+      <input className="profile__name editable" placeholder="Сменить имя"
+        defaultValue={user ? user.name : ''}
+        onBlur={changeName}
+      />
       <h2>Пройденные тесты:</h2>
       {finished.map(result => result.test && (
         <div key={result.id}>{result.test.name} — {result.score * 100}%</div>
