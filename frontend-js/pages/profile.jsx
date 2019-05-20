16bc492d11dd6776c62da1beb9095558e35bef7e
MurkyMeow <dinosowermurky@gmail.com>
2019-05-20 18:56:51 +0900

    frontend-js/profile: a little redesign

--- frontend-js/pages/profile.jsx
+++ frontend-js/pages/profile.jsx
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import { useGlobalState } from '../index';
 import { get, useRequest, post } from '../api';
-import Button from '../components/button';
+import { TestCard } from '../components/test-card';
 
 const Profile = () => {
   const [user, setUser] = useGlobalState('user');
-  const [, tests = []] = useRequest(() => get('/test/tests?samples=5'));
+  const [, tests = []] = useRequest(() => get('/test/tests?samples=5&eager=questions'));
   const [, finished = []] = useRequest(() => get('/stats/tests'));
 
   const changeName = async e => {
@@ -20,22 +21,20 @@ const Profile = () => {
         defaultValue={user ? user.name : ''}
         onBlur={changeName}
       />
-      <h2>Пройденные тесты:</h2>
-      {finished.map(result => result.test && (
-        <div key={result.id}>{result.test.name} — {result.score * 100}%</div>
-      ))}
-      <h2>Тесты:</h2>
-      <div className="profile-tests">
+      {finished.length && <>
+        <h3>Пройденные тесты:</h3>
+        {finished.map(result => result.test && (
+          <Link href={`/test?id=${result.test_id}`} key={result.id}>
+            <div className="profile__finished-test" key={result.id}>
+              {result.test.name} — {result.score * 100}%
+            </div>
+          </Link>
+        ))}
+      </>}
+      <h3 className="profile__tests-title">Опубликованные тесты:</h3>
+      <div className="profile__tests">
         {tests.map(test => (
-          <div className="profile-test" key={test.id}>
-            <div className="profile-test-name">{test.name}</div>
-            <div className="profile-test-stats">
-              <i>remove_red_eye</i>20
-            </div>
-            <div className="profile-test-actions">
-              <Button className="profile-test-btn">Пройти</Button>
-            </div>
-          </div>
+          <TestCard className="profile__test" test={test} key={test.id}/>
         ))}
       </div>
     </div>
