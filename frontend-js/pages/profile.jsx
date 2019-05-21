1e4d319789deca5338fca4c15774d8cbec9f9c82
MurkyMeow <dinosowermurky@gmail.com>
2019-05-21 16:37:15 +0900

    frontend-js/profile: allow viewing not only users's own profile

--- frontend-js/pages/profile.jsx
+++ frontend-js/pages/profile.jsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
+import { withRouter } from 'next/router';
 import { useGlobalState } from '../index';
 import { get, useRequest, post } from '../api';
 import { TestCard } from '../components/test-card';
 
-const Profile = () => {
+const Profile = ({ router }) => {
   const [user, setUser] = useGlobalState('user');
-  const [, tests = []] = useRequest(() => get('/test/tests?samples=5&eager=questions'));
-  const [, finished = []] = useRequest(() => get('/stats/tests'));
+  const [loading, profile] = useRequest(() =>
+    get(`/stats/profile?id=${router.query.id || ''}`)
+  );
 
   const changeName = async e => {
     const { value } = e.target;
@@ -15,15 +17,17 @@ const Profile = () => {
       setUser({ ...user, name: value });
     }
   };
+  if (loading) return <div className="page-title">Loading...</div>;
   return (
     <div className="profile">
       <input className="profile__name editable" placeholder="Сменить имя"
-        defaultValue={user ? user.name : ''}
+        disabled={user && user.id !== profile.id}
+        defaultValue={profile.name || profile.id}
         onBlur={changeName}
       />
-      {finished.length && <>
+      {profile.results.length > 0 && <>
         <h3>Пройденные тесты:</h3>
-        {finished.map(result => result.test && (
+        {profile.results.map(result => result.test && (
           <Link href={`/test?id=${result.test_id}`} key={result.id}>
             <div className="profile__finished-test" key={result.id}>
               {result.test.name} — {result.score * 100}%
@@ -31,14 +35,16 @@ const Profile = () => {
           </Link>
         ))}
       </>}
-      <h3 className="profile__tests-title">Опубликованные тесты:</h3>
-      <div className="profile__tests">
-        {tests.map(test => (
-          <TestCard className="profile__test" test={test} key={test.id}/>
-        ))}
-      </div>
+      {profile.tests.length > 0 && <>
+        <h3 className="profile__tests-title">Опубликованные тесты:</h3>
+        <div className="profile__tests">
+          {profile.tests.map(test => (
+            <TestCard className="profile__test" test={test} key={test.id}/>
+          ))}
+        </div>
+      </>}
     </div>
   );
 };
 
-export default Profile;
+export default withRouter(Profile);
