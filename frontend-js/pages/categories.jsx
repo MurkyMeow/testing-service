2bcc3024853b18d2b59947bd9d479b853a2aade1
MurkyMeow <dinosowermurky@gmail.com>
2019-05-21 20:26:28 +0900

    frontend/categories: use `Editable` and actually change category title

--- frontend-js/pages/categories.jsx
+++ frontend-js/pages/categories.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import { useDocument, canEdit } from '../index';
-import { useRequest, get } from '../api';
+import { useRequest, get, post } from '../api';
+import { useNotification } from '../components/notification';
+import { Editable } from '../components/editable';
 import Button from '../components/button';
 
 const Test = ({ test, finished }) => (
@@ -11,43 +13,52 @@ const Test = ({ test, finished }) => (
   </div>
 );
 
-const Category = ({ category, stats, onRemove }) => (
-  <div className="page-categories__category">
-    <header className="page-categories__category-header">
-      <input className="page-categories__category-name editable"
-        defaultValue={category.name}
-        disabled={!canEdit(category)}
-      />
-      {canEdit(category) && (
-        <i className="page-categories__category-delete-btn" onClick={onRemove}>
-            close
-        </i>
-      )}
-    </header>
-    <div className="page-categories__category-test-list">
-      {category.tests.map(test => (
-        <Test test={test} key={test.id}
-          finished={stats.find(x => x.test && x.test.id === test.id)}
+const Category = ({ category, stats, onRemove }) => {
+  const [notification, notify] = useNotification();
+  const changeName = name => {
+    post('/test/categories', { id: category.id, name })
+      .catch(() => notify('error', 'Не удалось поменять название'));
+  };
+  return (
+    <div className="page-categories__category">
+      {notification}
+      <header className="page-categories__category-header">
+        <Editable className="page-categories__category-name"
+          initial={category.name}
+          disabled={!canEdit(category)}
+          onChange={changeName}
         />
-      ))}
-      <Link href={`/test_edit?category_id=${category.id}`}>
-        <div className="page-categories__test-add-btn">Добавить тест</div>
-      </Link>
-    </div>
-    <div className="page-categories__category-summary">
-      <div><i>info</i>6</div>
-      <div><i>query_builder</i>60</div>
-      {category.creator && (
-        <Link href={`/profile?id=${category.creator.id}`}>
-          <div className="page-categories__category-creator">
-            Создатель: {category.creator && category.creator.name}
-          </div>
+        {canEdit(category) && (
+          <i className="page-categories__category-delete-btn" onClick={onRemove}>
+            close
+          </i>
+        )}
+      </header>
+      <div className="page-categories__category-test-list">
+        {category.tests.map(test => (
+          <Test test={test} key={test.id}
+            finished={stats.find(x => x.test && x.test.id === test.id)}
+          />
+        ))}
+        <Link href={`/test_edit?category_id=${category.id}`}>
+          <div className="page-categories__test-add-btn">Добавить тест</div>
         </Link>
-      )}
-      <Button link={`/category?id=${category.id}`}>Перейти</Button>
+      </div>
+      <div className="page-categories__category-summary">
+        <div><i>info</i>6</div>
+        <div><i>query_builder</i>60</div>
+        {category.creator && (
+          <Link href={`/profile?id=${category.creator.id}`}>
+            <div className="page-categories__category-creator">
+              Создатель: {category.creator && category.creator.name}
+            </div>
+          </Link>
+        )}
+        <Button link={`/category?id=${category.id}`}>Перейти</Button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const Categories = () => {
   const [name, setName] = useState('');
