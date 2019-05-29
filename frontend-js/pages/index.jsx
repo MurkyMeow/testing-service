6ac9c753e0299f505506144031e2b76089ef9c7d
unknown <ckieverentineas@yandex.ru>
2019-05-29 15:35:48 +1000

    frontend/index: added information

--- frontend-js/pages/index.jsx
+++ frontend-js/pages/index.jsx
@@ -1,5 +1,22 @@
 const Index = (() => (
-  <h1 className="page-title">Главная страница</h1>
+  <div>
+    <h1 className="page-title">Интернет-платформа тестирования студентов</h1>
+    <div>
+      <p className="page-definition">
+        Система тестирования, предоставляет всем желающим
+        платформу для создания и прохождения тестов.
+      </p>
+    </div>
+    <h2 className="page-title">Возможности:</h2>
+    <div className="page-flex">
+      <div className="page-item">Кроссплатформенность</div>
+      <div className="page-item">Открытый исходный код</div>
+      <div className="page-item">Возможность создавать свои тесты</div>
+      <div className="page-item">Возможность редактировать свои тесты и удалять</div>
+      <div className="page-item">Обобщенность результатов по тестам</div>
+      <div className="page-item">Простота развертки системы тестирования в локальных сетях</div>
+    </div>
+  </div>
 ));
 
 export default Index;
