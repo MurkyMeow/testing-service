a171d8a9f3a15b0ac6a3a4c1f6bd2a45af98f94a
MurkyMeow <dinosowermurky@gmail.com>
2019-06-01 20:46:19 +0900

    adjust seed data to not break any constraints

--- seeds/conclusion.js
+++
@@ -1,6 +0,0 @@
-exports.seed = (knex, Promise) => Promise.all([
-  knex('conclusion').insert([
-    { test_id: 1, min_score: 0, text: 'Вы можете избегать конфликты, но могли бы их еще и предотвращать' },
-    { test_id: 1, min_score: 0.9, text: 'Любой конфликт не представляет для вас угрозы' },
-  ]),
-]);
