3e7e7d82b2dda67db2911d4e335f046c14c92506
MurkyMeow <dinosowermurky@gmail.com>
2019-06-04 18:51:51 +0900

    frontend-js/index: add some content

--- frontend-js/pages/index.jsx
+++ frontend-js/pages/index.jsx
@@ -1,5 +1,23 @@
-const Index = (() => (
-  <h1 className="page-title">Главная страница</h1>
-));
+const Index = () => (
+  <div className="page-index">
+    <div className="page-index__heading">
+      <h1>Интернет платформа</h1>
+      <h2>Тестирования студентов</h2>
+      <img className="page-index__logo" src="/logo.svg"/>
+    </div>
+    <div className="page-index__features-heading">Возможности</div>
+    <div className="page-index__features">
+      <div className="page-index__feature --f1">
+        Простая развёртка в локальных сетях
+      </div>
+      <div className="page-index__feature --f2">
+        Создание и редактирование тестов
+      </div>
+      <div className="page-index__feature --f3">
+        Обобщение результатов испытуемых
+      </div>
+    </div>
+  </div>
+);
 
 export default Index;
