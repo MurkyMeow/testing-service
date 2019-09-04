f2393945d752b637cf86209a3ec1d4b3fbc8040e
MurkyMeow <dinosowermurky@gmail.com>
2019-09-04 21:30:00 +0900

    apply type-graphql decorators to some more entities

--- src/entity/test.ts
+++ src/entity/test.ts
@@ -1,26 +1,36 @@
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, ManyToOne, OneToMany } from 'typeorm';
+import { Field, ID, ObjectType } from 'type-graphql';
+import { MinLength } from 'class-validator';
 import { Category } from './category';
 import { User } from './user';
 import { Question } from './question';
 import { Conclusion } from './conclusion';
 
 @Entity()
+@ObjectType()
 export class Test extends BaseEntity {
   @PrimaryGeneratedColumn()
+  @Field(() => ID)
   id: number;
 
   @Column()
+  @Field()
   name: string;
 
   @ManyToOne(() => User, user => user.tests)
+  @Field(() => User)
   creator: User;
 
   @ManyToOne(() => Category, category => category.tests)
+  @Field(() => Category)
   category: Category;
 
   @OneToMany(() => Question, question => question.test)
+  @Field(() => [Question])
+  @MinLength(1)
   questions: Question[];
 
   @OneToMany(() => Conclusion, conclusion => conclusion.test)
+  @Field(() => [Conclusion])
   conclusions: Conclusion[];
 }
