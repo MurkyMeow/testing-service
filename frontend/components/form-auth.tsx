28a4f08fa632798f28f1fc6b5b3bb8fa2fde6e01
MurkyMeow <dinosowermurky@gmail.com>
2020-01-23 18:42:17 +0900

    form-auth: refactor

--- frontend/components/form-auth.tsx
+++ frontend/components/form-auth.tsx
@@ -1,8 +1,7 @@
 import gql from 'graphql-tag';
-import { FormEvent } from 'react';
-import { post } from '../api';
+import { FormEvent, useCallback } from 'react';
 import { notify } from '../index';
-import Button from './button';
+import { Button } from './button';
 import css from './form-auth.css';
 import { useMutation } from '@apollo/react-hooks';
 
@@ -28,53 +27,47 @@ const SIGNIN = gql`
 `;
 
 export const AuthForm = (props: {
-  type: 'signup' | 'signup',
+  type: 'signup' | 'signup';
 }) => {
   const [signin] = useMutation(SIGNIN);
   const [signup] = useMutation(SIGNUP);
 
-  const handleSignin = async (e: FormEvent<HTMLFormElement>) => {
+  const handleSignin = useCallback(async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const data = new FormData(e.currentTarget);
     try {
       await signin({ variables: data });
     } catch (err) {
-      notify('error', err.status === 400 ? 'Неправильный логин или пароль' : 'Не удаётся войти. Попробуйте перезагрузить страницу.');
+      if (err.status !== 400) throw err;
+      notify('error', 'Неправильный логин или пароль');
     }
-  };
+  }, [signin]);
 
-  const handleSignup = async (e: FormEvent<HTMLFormElement>) => {
+  const handleSignup = useCallback(async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const data = new FormData(e.currentTarget);
-    try {
-      await signup({ variables: data });
-    } catch (err) {
-      notify('Не удаётся войти. Попробуйте перезагрузить страницу.');
-    }
-  };
+    await signup({ variables: data });
+  }, [signup]);
 
-  const submit = async (e: FormEvent<HTMLFormElement>) => {
+  const submit = useCallback(async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const data = new FormData(e.currentTarget);
     try {
-      const endpoint = signup ? '/auth/signup' : '/auth/signin';
-      const user = await post(endpoint, {
-        email: data.get('email'),
-        password: data.get('password'),
-      });
+      if (props.type === 'signup') {
+        await handleSignup(e);
+      } else {
+        await handleSignin(e);
+      }
     } catch (err) {
-      if (err.status === 400) notify('error', 'Неправильный логин или пароль');
-      else notify('error', 'Не удаётся войти. Попробуйте перезагрузить страницу.');
+      notify('error', 'Не удаётся войти. Попробуйте перезагрузить страницу.');
     }
-  };
+  }, [handleSignin, handleSignup, props.type]);
 
   return (
     <form className={css.auth} onSubmit={submit}>
-      <div className={css.auth__header}>Заполните поля</div>
-      <input className={css.auth__field} name="email" type="email" placeholder="Email" required/>
-      <input className={css.auth__field} name="password" type="password" placeholder="Пароль" required/>
-      )}
-      <Button className={css.auth__btn}>Войти</Button>
+      <div className={css.header}>Заполните поля</div>
+      <input className={css.field} name="email" type="email" placeholder="Email" required/>
+      <input className={css.field} name="password" type="password" placeholder="Пароль" required/>
+      <Button className={css.submitBtn}>Войти</Button>
     </form>
   );
 };
