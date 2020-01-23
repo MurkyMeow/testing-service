807962fc9c63b3e8633a9150f099890d058cf758
MurkyMeow <dinosowermurky@gmail.com>
2020-01-24 07:40:36 +0900

    page/moderation: refactor

--- frontend/pages/moderation.tsx
+++ frontend/pages/moderation.tsx
@@ -1,41 +1,36 @@
 import Link from 'next/link';
+import { FormEvent } from 'react';
 import { notify } from '../index';
 import { useRequest, get, post } from '../api';
-import Button from '../components/button';
+import { Button } from '../components/button';
 import css from './moderation.css';
 
-export default () => {
+export default function Moderation() {
   const [, teachers = [], setTeachers] = useRequest((() =>
     get('/stats/profiles?role=teacher')
   ));
-  const submit = async e => {
+
+  const submit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const [$id] = e.target;
+    const data = new FormData(e.currentTarget);
+    const id = Number(data.get('id'));
+    if (teachers.some(x => x.id === id)) {
+      return notify('error', 'Данный пользователь уже присутствует в списке');
+    }
     try {
-      if (teachers.find(x => String(x.id) === $id.value)) {
-        notify('error', 'Данный пользователь уже присутствует в списке');
-      } else {
-        const teacher = await post('/stats/assign', {
-          id: $id.value,
-          role: 'teacher',
-        });
-        setTeachers([...teachers, teacher]);
-      }
+      const teacher = await post('/stats/assign', { id, role: 'teacher' });
+      setTeachers([...teachers, teacher]);
     } catch (err) {
       switch (err.status) {
-        case 404:
-          notify('error', 'Указанный пользователь не найден');
-          break;
-        case 400:
-          notify('error', 'Нельзя поменять собственную роль');
-          break;
-        default:
-          notify('error', 'Не удалось назначить пользователя');
+        case 404: return notify('error', 'Указанный пользователь не найден');
+        case 400: return notify('error', 'Нельзя поменять собственную роль');
+        default: return notify('error', 'Не удалось назначить пользователя');
       }
     }
-    $id.value = '';
+    e.currentTarget.reset();
   };
-  const unassign = async id => {
+
+  const unassign = async (id: number) => {
     try {
       await post('/stats/assign', { id, role: '' });
       setTeachers(teachers.filter(x => x.id !== id));
@@ -43,13 +38,14 @@ export default () => {
       notify('error', 'Не удалось разжаловать пользователя');
     }
   };
+
   return (
     <div className={css.pageModeration}>
       <h2>Список преподавателей:</h2>
       {teachers.map(t => (
         <div className={css.teacher} key={t.id}>
           <Link href={`/profile?id=${t.id}`}>
-            <a href="#">{t.name || 'Пользователь'} (id{t.id})</a>
+            <a>{t.name || 'Пользователь'} (id{t.id})</a>
           </Link>
           <Button onClick={() => unassign(t.id)}>Разжаловать</Button>
         </div>
@@ -60,4 +56,4 @@ export default () => {
       </form>
     </div>
   );
-};
+}
