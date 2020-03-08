2b1996660aecee1872b0e174923cf0d695fbab43
meowlaika <dinosowermurky@gmail.com>
2020-03-08 18:56:22 +0700

    button: use <a> tag when the user wants a link

--- frontend/components/button.tsx
+++ frontend/components/button.tsx
@@ -4,28 +4,30 @@ import Link from 'next/link';
 
 import './button.css';
 
-export const Button = (props: {
+export function Button(props: {
   className?: string;
   link?: string;
   onClick?: () => void;
   disabled?: boolean;
   variant?: 'primary' | 'secondary';
   children?: ReactNode | ReactNode[];
-}) => {
-  const button = (
-    <button
-      className={cx(
-        props.className,
-        'button',
-        `button_${props.variant}`,
-        props.disabled && 'button_disabled',
-      )}
-      onClick={props.onClick}
-    >
+}) {
+  const inner = (
+    <span className={cx(
+      props.className,
+      'button',
+      `button_${props.variant}`,
+      props.disabled && 'button_disabled',
+    )}>
       {props.children}
-    </button>
+    </span>
   );
-  return props.link
-    ? <Link href={props.link}>{button}</Link>
-    : button;
-};
+  if (props.link === undefined) {
+    return <button className="button-wrap">{inner}</button>;
+  }
+  return (
+    <Link href={props.link}>
+      <a className="button-wrap">{inner}</a>
+    </Link>
+  );
+}
