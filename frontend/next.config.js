3788d698350d8d1b94e5ce1eb3ae2584949053af
meowlaika <dinosowermurky@gmail.com>
2020-03-09 18:39:26 +0700

    fix css imports, fix eslint

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+
 const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]___[hash:base64:8]",
-  },
 });
--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 module.exports = {
   plugins: {
     'postcss-preset-env': {
