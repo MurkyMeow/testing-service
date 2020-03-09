09bc8b35126571045195c36a7a389354981022eb
meowlaika <dinosowermurky@gmail.com>
2020-03-09 18:27:09 +0700

    _app: drop deprecated <Container>

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,11 @@
-import App, { Container } from 'next/app';
+import App from 'next/app';
+import fetch from 'cross-fetch';
 import ApolloClient from 'apollo-client';
 import { Provider as ReduxProvider } from 'react-redux';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { createUploadLink } from 'apollo-upload-client';
-import { Notification } from '../components/notification';
+import { Toast } from '../components/notification';
 import { Header } from '../components/header';
 import { store } from '../store';
 
@@ -15,23 +16,21 @@ const uri = process.env.production
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: createUploadLink({ uri, credentials: 'include' }),
+  link: createUploadLink({ uri, fetch, credentials: 'include' }),
 });
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Container>
-        <ReduxProvider store={store}>
-          <ApolloProvider client={client}>
-            <Notification />
-            <Header />
-            <Component {...pageProps} />
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-          </ApolloProvider>
-        </ReduxProvider>
-      </Container>
+      <ReduxProvider store={store}>
+        <ApolloProvider client={client}>
+          <Toast />
+          <Header />
+          <Component {...pageProps} />
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        </ApolloProvider>
+      </ReduxProvider>
     );
   }
 }
